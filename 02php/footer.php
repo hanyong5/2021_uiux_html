bfe9816dab12270640a897d47da0887423040148
hanyong5 <hanyong5@naver.com>
2021-09-08 11:58:46 +0900

    css menu

---
+++ 02php/footer.php
@@ -0,0 +1,5 @@
+    <footer id="footer">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, architecto.
+    </footer>
+</body>
+</html>
---
+++ 02php/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header id="header">
+        <h1><a href="#">logo</a></h1>
+        <nav class="gnb">
+            <ul class="clearfix">
+                <li><a href="#">menu1</a></li>
+                <li><a href="#">menu2</a></li>
+                <li><a href="#">menu3</a></li>
+                <li><a href="#">menu4</a></li>
+                <li><a href="#">menu5</a></li>
+                <li><a href="#">menu6</a></li>
+            </ul>
+        </nav>
+    </header>
