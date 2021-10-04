5902071e172cb8a6a5f87a810c825a7b7b0640b3
hanyong5 <hanyong5@naver.com>
2021-10-04 15:48:22 +0900

    theme 1st

---
+++ 00theme/basic/head.php
@@ -0,0 +1,131 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+if (G5_IS_MOBILE) {
+    include_once(G5_THEME_MOBILE_PATH.'/head.php');
+    return;
+}
+
+if(G5_COMMUNITY_USE === false) {
+    define('G5_IS_COMMUNITY_PAGE', true);
+    include_once(G5_THEME_SHOP_PATH.'/shop.head.php');
+    return;
+}
+include_once(G5_THEME_PATH.'/head.sub.php');
+include_once(G5_LIB_PATH.'/latest.lib.php');
+include_once(G5_LIB_PATH.'/outlogin.lib.php');
+include_once(G5_LIB_PATH.'/poll.lib.php');
+include_once(G5_LIB_PATH.'/visit.lib.php');
+include_once(G5_LIB_PATH.'/connect.lib.php');
+include_once(G5_LIB_PATH.'/popular.lib.php');
+?>
+
+<!-- 상단 시작 { -->
+<div id="hd">
+    <h1 id="hd_h1"><?php echo $g5['title'] ?></h1>
+    <div id="skip_to_container"><a href="#container">본문 바로가기</a></div>
+
+    <?php
+    if(defined('_INDEX_')) { // index에서만 실행
+        include G5_BBS_PATH.'/newwin.inc.php'; // 팝업레이어
+    }
+    ?>
+    
+    <div id="hd_wrapper">
+
+        <div id="logo">
+            <a href="<?php echo G5_URL ?>"><img src="<?php echo G5_THEME_IMG_URL ?>/logo/logo.png" alt="<?php echo $config['cf_title']; ?>"></a>
+        </div>
+
+
+		<nav id="gnb">
+        <h2>메인메뉴</h2>
+        <div class="gnb_wrap">
+            <ul id="gnb_1dul">
+                
+                <?php
+				$menu_datas = get_menu_db(0, true);
+				$gnb_zindex = 999; // gnb_1dli z-index 값 설정용
+                $i = 0;
+                foreach( $menu_datas as $row ){
+                    if( empty($row) ) continue;
+                    $add_class = (isset($row['sub']) && $row['sub']) ? 'gnb_al_li_plus' : '';
+                ?>
+                <li class="gnb_1dli <?php echo $add_class; ?>" style="z-index:<?php echo $gnb_zindex--; ?>">
+                    <a href="<?php echo $row['me_link']; ?>" target="_<?php echo $row['me_target']; ?>" class="gnb_1da"><?php echo $row['me_name'] ?></a>
+                    <?php
+                    $k = 0;
+                    foreach( (array) $row['sub'] as $row2 ){
+
+                        if( empty($row2) ) continue; 
+
+                        if($k == 0)
+                            echo '<span class="bg">하위분류</span><div class="gnb_2dul"><ul class="gnb_2dul_box">'.PHP_EOL;
+                    ?>
+                        <li class="gnb_2dli"><a href="<?php echo $row2['me_link']; ?>" target="_<?php echo $row2['me_target']; ?>" class="gnb_2da"><?php echo $row2['me_name'] ?></a></li>
+                    <?php
+                    $k++;
+                    }   //end foreach $row2
+
+                    if($k > 0)
+                        echo '</ul></div>'.PHP_EOL;
+                    ?>
+                </li>
+                <?php
+                $i++;
+                }   //end foreach $row
+
+                if ($i == 0) {  ?>
+                    <li class="gnb_empty">메뉴 준비 중입니다.<?php if ($is_admin) { ?> <a href="<?php echo G5_ADMIN_URL; ?>/menu_list.php">관리자모드 &gt; 환경설정 &gt; 메뉴설정</a>에서 설정하실 수 있습니다.<?php } ?></li>
+                <?php } ?>
+            </ul>
+            
+            
+        </div>
+    </nav>
+    
+        
+        
+    </div>
+    
+    
+
+</div>
+<!-- } 상단 끝 -->
+
+
+<hr>
+
+<!-- 콘텐츠 시작 { -->
+<div id="wrapper">
+	
+    <div id="container_wr">
+	
+	
+	
+	<!-- 메인페이지는 보이고 서브페이지는 안보이게 만듬 -->
+	<? if( defined("_INDEX_") ){ ?>
+
+
+		<link rel="stylesheet" href="<? echo G5_THEME_URL; ?>/css/jquery.bxslider.css">
+		<script>
+			$(document).ready(function(){
+				$(".slider").bxSlider();
+			})
+		</script>
+		<div>
+			<ul class="slider">
+				<li><img src="<? echo G5_THEME_URL; ?>/img/pc01.jpg" alt=""></li>
+				<li><img src="<? echo G5_THEME_URL; ?>/img/pc02.jpg" alt=""></li>
+				<li><img src="<? echo G5_THEME_URL; ?>/img/pc03.jpg" alt=""></li>
+			</ul>		
+				
+		</div>
+
+
+	<? } ?>
+
+    
+	
+	<div id="container">
+        <?php if (!defined("_INDEX_")) { ?><h2 id="container_title"><span title="<?php echo get_text($g5['title']); ?>"><?php echo get_head_title($g5['title']); ?></span></h2><?php }
---
+++ 00theme/basic/index.php
@@ -0,0 +1,24 @@
+<?php
+if (!defined('_INDEX_')) define('_INDEX_', true);
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+if (G5_IS_MOBILE) {
+    include_once(G5_THEME_MOBILE_PATH.'/index.php');
+    return;
+}
+
+if(G5_COMMUNITY_USE === false) {
+    include_once(G5_THEME_SHOP_PATH.'/index.php');
+    return;
+}
+
+include_once(G5_THEME_PATH.'/head.php');
+?>
+
+<? echo G5_THEME_PATH  ?>
+
+<div style="height: 200px;background:pink"	></div>		
+
+
+<?php
+include_once(G5_THEME_PATH.'/tail.php');
