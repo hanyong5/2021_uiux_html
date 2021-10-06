f1cd82f4665422c9e7076376d97384afda035306
hanyong5 <hanyong5@naver.com>
2021-10-06 12:02:52 +0900

    menu v3

--- 01javascript/20211006_barMenu/js/script.js
+++ 01javascript/20211006_barMenu/js/script.js
@@ -3,8 +3,29 @@ function nowMenu(menu){ // menu = "main"
         $(".bar").css("opacity",0)
         console.log("메인연결됨");
     }else{
+        $(".menu li").eq(menu).addClass("active") // document.querySelectorAll(".menu li")[menu].classList.add("active")
         $(".bar").css("left",200*menu)
         console.log("서브페이지")
     }
+
+    //$(".menu li").hover(function(){},function(){})
+
+    $(".menu li").hover(function(){
+        let menuLi = $(this).index()
+
+        if(menu == "main"){
+            $(".bar").css("opacity",1);
+        }
+
+        $(".bar").css("left",200*menuLi)
+    },function(){
+        if(menu == "main"){
+            $(".bar").css("opacity",0)
+        }
+        $(".bar").css("left",200*menu)
+    });
+
+
 }
+
 
