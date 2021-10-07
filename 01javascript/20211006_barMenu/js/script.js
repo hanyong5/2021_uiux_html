997a6a58e84fd2df5c7b4a8de6e29a8818e3eac6
hanyong5 <hanyong5@naver.com>
2021-10-07 12:20:40 +0900

    car v1

--- 01javascript/20211006_barMenu/js/script.js
+++ 01javascript/20211006_barMenu/js/script.js
@@ -1,9 +1,10 @@
-function nowMenu(menu){ // menu = "main"
+function nowMenu(menu){ // menu = "main"  menu = 0
     if(menu == "main"){
         $(".bar").css("opacity",0);
         console.log("메인연결됨");
     }else{
-        $(".menu li").eq(menu).addClass("active"); // document.querySelectorAll(".menu li")[menu].classList.add("active")
+        $(".menu li").eq(menu).addClass("active"); 
+        // document.querySelectorAll(".menu li")[menu].classList.add("active")
         $(".bar").css("left",200*menu);
         console.log("서브페이지");
     }
@@ -12,23 +13,14 @@ function nowMenu(menu){ // menu = "main"
 
     $(".menu li").hover(function(){
         let menuLi = $(this).index();
-        
-        $(this).css("background-color","skyblue");
-
-        if(menu == "main"){
-            $(".bar").css("opacity",1);
-        }
-
-        $(".bar").css("left",200*menuLi);
-        
+        if(menu == "main"){$(".bar").css("opacity",1)}
+        $(".bar").css("left",200*menuLi)
     },function(){
-        $(this).css("background-color","initial");
-        if(menu == "main"){
-            $(".bar").css("opacity",0);
-        }
-        $(".bar").css("left",200*menu);
+        if(menu == "main"){$(".bar").css("opacity",0)}
+        $(".bar").css("left",200*menu)
+    })
 
-    });
+    
 
 
 }
