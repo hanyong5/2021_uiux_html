217fe729107ccacce89fac97ca2c2acf903d91cf
hanyong5 <hanyong5@naver.com>
2021-10-07 14:25:15 +0900

    theme v6

--- 00theme/basic/skin/latest/basic/latest.skin.php
+++ 00theme/basic/skin/latest/basic/latest.skin.php
@@ -3,6 +3,9 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
 add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 0);
+
+$thumb_width = 239;
+$thumb_height = 150;
 $list_count = (is_array($list) && $list) ? count($list) : 0;
 ?>
 
@@ -12,7 +15,22 @@ $list_count = (is_array($list) && $list) ? count($list) : 0;
 		<a href="<?php echo get_pretty_url($bo_table); ?>" class="btn">btn</a>
 	</div>
 
-    <?php for ($i=0; $i<$list_count; $i++) {  ?>
+    <?php for ($i=0; $i<$list_count; $i++) {  
+		
+	$thumb = get_list_thumbnail($bo_table, $list[$i]['wr_id'], $thumb_width, $thumb_height, false, true);	
+	
+	if($thumb['src']) {
+        $img = $thumb['src'];
+    } else {
+        $img = G5_IMG_URL.'/no_img.png';
+        $thumb['alt'] = '이미지가 없습니다.';
+    }	
+	$img_content = '<img src="'.$img.'" alt="'.$thumb['alt'].'" >';
+
+
+	?>
+	
+
 		<div class="content">
 
 
@@ -26,6 +44,9 @@ $list_count = (is_array($list) && $list) ? count($list) : 0;
 
 				echo "</a>";
 				?>
+			</div>
+			<div class="img">
+				<?php echo run_replace('thumb_image_tag', $img_content, $thumb); ?>
 			</div>
 			
 			<div class="etc">
